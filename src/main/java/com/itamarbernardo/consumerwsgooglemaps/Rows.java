8c18f058d823492e07c28c218b15e82f27818b60
Itamar Bernardo <itamarbernardo2013@gmail.com>
2016-12-03 23:53:06 -0300

    First commit

---
+++ src/main/java/com/itamarbernardo/consumerwsgooglemaps/Rows.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itamarbernardo.consumerwsgooglemaps;
+
+/**
+ *
+ * @author ANAFLAVIA
+ */
+class Rows {
+    
+    private Elementos elementos;
+
+    public Rows(Elementos elementos) {
+        this.elementos = elementos;
+    }
+    
+    public Elementos getElementos() {
+        return elementos;
+    }
+    
+    public void setElementos(Elementos elementos) {
+        this.elementos = elementos;
+    }
+
+    
+    
+    
+    
+}
